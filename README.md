ac31f0e8034718f63e5db6830d2eafda7d8d5bcd
Jessica Stodola <Jessica_Stodola@comcast.com>
2019-03-08 12:25:36 -0600

    Plugin to embed the content of a specified markdown file within another markdown file

---
+++ README.md
@@ -0,0 +1,61 @@
+# gatsby-remark-embed-markdown
+
+Embeds the contents of specified files within Prism-formatted HTML blocks.
+
+## Overview
+
+### Embedding Files
+
+For example, given the following project directory structure:
+
+```
+./examples/
+├── sample-markdown-file.md
+```
+
+The following markdown syntax is used to embed the contents of these files:
+
+```md
+# Sample Markdown
+
+`markdown:sample-markdown-file.md`
+
+```
+
+The resulting HTML for the above markdown would look something like this:
+
+```html
+<h1>Sample Markdown</h1>
+<div class="markdown-fragment">
+  <!-- Embedded content here ... -->
+</div>
+```
+
+## Installation
+
+`npm install --save gatsby-remark-embed-markdown`
+
+## How to use
+
+```js
+// In your gatsby-config.js
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-embed-markdown",
+            options: {
+              // Example code links are relative to this dir.
+              // eg examples/path/to/file.js
+              directory: `${__dirname}/examples/`,
+            }
+          }
+        ]
+      }
+    }
+  ]
+}
+```
---
+++ index.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const normalizePath = require("normalize-path");
+const visit = require("unist-util-visit");
+const unified = require('unified');
+const parse = require('remark-parse');
+const html = require('remark-html');
+
+module.exports = function (_ref, _temp) {
+  var markdownAST = _ref.markdownAST;
+
+  let _ref2 = _temp === void 0 ? {} : _temp,
+    directory = _ref2.directory;
+
+  if (!directory) {
+    throw Error(`Required option \"directory\" not specified`);
+  } else if (!fs.existsSync(directory)) {
+    throw Error(`Invalid directory specified \"${ directory }\"`);
+  } else if (!directory.endsWith("/")) {
+    directory += "/";
+  }
+
+  visit(markdownAST, "inlineCode", function (node) {
+    const value = node.value;
+
+    if (value.startsWith("markdown:")) {
+      const file = value.substr(9);
+      const path = normalizePath("" + directory + file);
+
+      if (!fs.existsSync(path)) {
+        throw Error(`Invalid fragment specified; no such file "${ path }"`);
+      }
+
+      const code = fs.readFileSync(path, "utf8");
+
+      const markdown = unified().use(parse).use(html);
+
+      try {
+        node.value = `<div class=\"markdown-fragment\">${ markdown.processSync(code) }</div>`;
+        node.type = "html";
+      } catch (e) {
+        throw Error(`${ e.message } \nFile: ${ file }`);
+      }
+    }
+  });
+  return markdownAST;
+};
